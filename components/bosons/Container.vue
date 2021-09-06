37f8f9c15f692863360b8395388e2b7ed1513a97
vonmalves <ismael.only@gmail.com>
2021-09-06 07:40:06 -0300

    Component Container

---
+++ components/bosons/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .container {
+    margin: 0 auto;
+    max-width: 1070px;
+    @include screen('small') {
+      max-width: 90%;
+    }
+    @include screen ('medium') {
+      max-width: 85%;
+    }
+    @include screen ('large, infinity') {
+      max-width: 70%;
+    }
+  }
+</style>
---
+++ layouts/ibook.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="ibook-layout">
+    <!-- Header -->
+    <Container>
+      <Nuxt />
+    </Container>
+    <!-- Footer -->
+  </div>
+</template>
+
+<style scoped>
+
+</style>
