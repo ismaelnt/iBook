2442a9fb771101ed9f3dadd82b77298afeebbe68
vonmalves <ismael.only@gmail.com>
2021-09-08 07:41:15 -0300

    book cover in page details

---
+++ components/organisms/BookDetails.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="book-details">
+    <BookCover />
+    <BookInfo />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/book/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <BookDetailsTemplate />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { books } from '@/store'
+
+  export default Vue.extend({
+    async asyncData({params}) {
+      await books.show({id: params.id as any})
+      console.log(books.$single)
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
