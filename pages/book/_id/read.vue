c559580735997dc7a74e7c62e1d0e078d5c29113
vonmalves <ismael.only@gmail.com>
2021-11-08 07:48:24 -0300

    README

---
+++ pages/book/_id/read.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Ler Livros</h2>
+
+    <BookReaderTemplate />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { books } from '@/store'
+
+  export default Vue.extend({
+    layout: 'ibook',
+    async asyncData({params}) {
+      await books.show({ id: params.id as any })
+    }
+  })
+</script>
